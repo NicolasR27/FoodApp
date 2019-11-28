dd96e42e8458a31397e14a70d1e2d87bbc515e90
Nicolas Rios <nickrios33@yahoo.com>
2019-11-28 13:54:21 -0800

    Initial Commit

---
+++ FoodApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by Nicolas Rios on 11/28/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+        Text("Hello, World!")
+        Text("Hello, World!")
+        
+        }
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
