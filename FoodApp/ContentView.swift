486df1f93b9213678f015cea77f65733ecc03209
Nicolas Rios <nickrios33@yahoo.com>
2019-11-28 14:16:25 -0800

    Added quality Ui button

--- FoodApp/ContentView.swift
+++ FoodApp/ContentView.swift
@@ -9,10 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let BunTypes = ["patato roll", "sesame see bun"]
+    let cheeseTypes = ["American","PerperJack","Swiss"]
+    let proteinTypes = ["Beef","Chicken","Veggie"]
+    
+    @State var Quantity: Int = 0
+    
     var body: some View {
         Form {
-        Text("Hello, World!")
-        Text("Hello, World!")
+        Text("Order Awesome Burger")
+            .font(.title)
+        Text("Is the Best Burger There!")
+            
+            // Quanity
+            Stepper("Qauntity",value: $Quantity)
         
         }
        
