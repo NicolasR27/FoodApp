9c8af18bd499bad64b9e104975404862f4492351
Nicolas Rios <nickrios33@yahoo.com>
2019-11-28 16:13:56 -0800

    changes to Project

--- FoodApp/ContentView.swift
+++ FoodApp/ContentView.swift
@@ -10,28 +10,84 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let BunTypes = ["patato roll", "sesame see bun"]
+    let bunTypes: [String] = ["Patato roll", "SwissBun","SourDough"]
     let cheeseTypes = ["American","PerperJack","Swiss"]
     let proteinTypes = ["Beef","Chicken","Veggie"]
     
-    @State var Quantity: Int = 0
+    @State var quantity: Int = 0
+    @State var bun: String = ""
+    @State var cheese:String = ""
+    @State var protein:String = ""
+    @State var specialInstructions: String = ""
     
     var body: some View {
-        Form {
-        Text("Order Awesome Burger")
-            .font(.title)
-        Text("Is the Best Burger There!")
+        NavigationView {
+            Form {
+                Text("Order Awesome Burger")
+                    .font(.title)
+                Text("is the Best Burger There!")
+                
+                // Quanity
+                Section(header:Text("Quantity")){
+                    Stepper(value: $quantity){
+                        Text(quantity.description)
+                    }
+                }
+                
+                Section(header:Text("options")){
+                    Picker(selection: $bun, label: Text("Bun")) {
+                        ForEach(bunTypes,id: \.self) { (bun) in
+                            Text(bun)
+                          }
+                        }  
+                        
+                       Picker(selection: $cheese, label: Text("cheese")) {
+                            ForEach(bunTypes,id: \.self) { (cheese) in
+                                Text(cheese)
+                                
+                            }
+                            
+                            
+                            
+                        }
+                        
+                        
+                        Picker(selection: $protein, label: Text("protein")) {
+                            ForEach(bunTypes,id: \.self) { (protein) in
+                                Text(protein)
+                                
+                            }
+                            
+                            
+                            
+                        }
+                        
+                        
+                        
+                        
+                    }
+                    Section(header:Text("Special Instructions")) {
+                        TextField("special instructions",text:$specialInstructions)
+                    }
+                    
+                    Button(action: submitOrder) {
+                        Text("submit Order")
+                        
+                    }
+                }
+                
+            }
             
-            // Quanity
-            Stepper("Qauntity",value: $Quantity)
-        
         }
-       
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+        
+              func submitOrder() {
+                //do something
+            }
+        }
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+    
